9d02f68dbbaf5709fc24fe78b841dc521996d7f8
viniciusvsjesus <viniciusvsprofissional@gmail.com>
2023-03-10 14:25:31 -0300

    ~melhorando aspectos

--- src/main/java/com/gama/livro/model/Livro.java
+++ src/main/java/com/gama/livro/model/Livro.java
@@ -8,52 +8,62 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="livro")
+@Table(name = "livro")
 public class Livro {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-private int id;
-	
-	@Column(nullable = false, length =50 )
+	@Column(name="îd")
+	private int id;
+
+	@Column(nullable = false, length = 50, name="titulo")
 	private String titulo;
-	@Column(length = 50)
+	@Column(length = 50, name="autor")
 	private String autor;
-	
+
+	@Column(name="ano")
 	private int ano;
-	@Column(length = 20)
+	@Column(length = 20, name="genero")
 	private String genero;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public int getAno() {
 		return ano;
 	}
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
+
 	public String getGenero() {
 		return genero;
 	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
-	
-	
-	
+
 }
--- src/main/java/com/gama/livro/service/ILivroService.java
+++ src/main/java/com/gama/livro/service/ILivroService.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import com.gama.livro.model.Livro;
 
 public interface ILivroService {
-public ArrayList<Livro> recuperarTodos();
-public Livro recuperarPorCodigo(int id);
-public Livro cadastrarNovoLivro(Livro livro);
+	public ArrayList<Livro> recuperarTodos();
+
+	public Livro recuperarPorCodigo(int id);
+
+	public Livro cadastrarNovoLivro(Livro livro);
 }
